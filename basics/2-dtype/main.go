4c67365f7db7e3df2908ed74b49be48cab8aa1c0
khekrncs <kishore.karunakaran@creditsaison-in.com>
2025-05-19 11:50:13 +0530

    Understanding default word size and default int type and zero division

--- basics/2-dtype/main.go
+++ basics/2-dtype/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"unsafe"
+)
 
 func main() {
 	var a int = 10            // Signed integer
@@ -9,4 +13,17 @@ func main() {
 	var d complex128 = 1 + 4i // Complex number
 
 	fmt.Println(a, b, c, d)
+	fmt.Println("- GOARCH : ", runtime.GOARCH)
+	fmt.Println("- int size : ", unsafe.Sizeof(int(10)))
+
+	bin := 0b_1010_1001
+	hex := 0x_1_2_3_4_5_6_7_8
+	oct := 0o_12
+
+	fmt.Println("Binary = ", bin)
+	fmt.Println("Hex = ", hex)
+	fmt.Println("Oct = ", oct)
+
+	div := 0
+	fmt.Println(10 / div)
 }
